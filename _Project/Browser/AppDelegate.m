bf5bab9ad01e30e79bdf8784b57ca8beb1f2a9be
jvanakker <jva@logic-design.nl>
2026-03-03 15:56:15 +0100

    General cleanup - fixed warnings and updated with tvOS 26 SDK

--- _Project/Browser/AppDelegate.m
+++ _Project/Browser/AppDelegate.m
@@ -14,6 +14,39 @@
 
 @implementation AppDelegate
 
+- (void)restoreCookiesFromDefaults {
+    NSData *cookieData = [[NSUserDefaults standardUserDefaults] objectForKey:@"ApplicationCookie"];
+    if (cookieData.length == 0) {
+        return;
+    }
+    
+    NSError *error = nil;
+    NSSet *allowedClasses = [NSSet setWithObjects:[NSArray class], [NSHTTPCookie class], nil];
+    NSArray *cookies = [NSKeyedUnarchiver unarchivedObjectOfClasses:allowedClasses
+                                                           fromData:cookieData
+                                                              error:&error];
+    if (![cookies isKindOfClass:[NSArray class]]) {
+        return;
+    }
+    
+    for (NSHTTPCookie *cookie in cookies) {
+        [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
+    }
+}
+
+- (void)saveCookiesToDefaults {
+    NSError *error = nil;
+    NSData *cookieData = [NSKeyedArchiver archivedDataWithRootObject:[[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]
+                                              requiringSecureCoding:NO
+                                                              error:&error];
+    if (cookieData == nil) {
+        return;
+    }
+    
+    [[NSUserDefaults standardUserDefaults] setObject:cookieData forKey:@"ApplicationCookie"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 	// Override point for customization after application launch.
@@ -29,59 +62,35 @@
         [[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"MobileMode"];
         [[NSUserDefaults standardUserDefaults] synchronize];
     }
-    NSData *cookieData = [[NSUserDefaults standardUserDefaults] objectForKey:@"ApplicationCookie"];
-    if ([cookieData length] > 0) {
-        NSArray *cookies = [NSKeyedUnarchiver unarchiveObjectWithData:cookieData];
-        for (NSHTTPCookie *cookie in cookies) {
-            [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
-        }
-    }
+    [self restoreCookiesFromDefaults];
 	return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
 	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    NSData *cookieData = [NSKeyedArchiver archivedDataWithRootObject:[[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]];
-    [[NSUserDefaults standardUserDefaults] setObject:cookieData forKey:@"ApplicationCookie"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    [self saveCookiesToDefaults];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
 	// Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
 	// If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    NSData *cookieData = [NSKeyedArchiver archivedDataWithRootObject:[[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]];
-    [[NSUserDefaults standardUserDefaults] setObject:cookieData forKey:@"ApplicationCookie"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    [self saveCookiesToDefaults];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
 	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    NSData *cookieData = [[NSUserDefaults standardUserDefaults] objectForKey:@"ApplicationCookie"];
-    if ([cookieData length] > 0) {
-        NSArray *cookies = [NSKeyedUnarchiver unarchiveObjectWithData:cookieData];
-        for (NSHTTPCookie *cookie in cookies) {
-            [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
-        }
-    }
+    [self restoreCookiesFromDefaults];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
 	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    NSData *cookieData = [[NSUserDefaults standardUserDefaults] objectForKey:@"ApplicationCookie"];
-    if ([cookieData length] > 0) {
-        NSArray *cookies = [NSKeyedUnarchiver unarchiveObjectWithData:cookieData];
-        for (NSHTTPCookie *cookie in cookies) {
-            [[NSHTTPCookieStorage sharedHTTPCookieStorage] setCookie:cookie];
-        }
-    }
+    [self restoreCookiesFromDefaults];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
 	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    NSData *cookieData = [NSKeyedArchiver archivedDataWithRootObject:[[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]];
-    [[NSUserDefaults standardUserDefaults] setObject:cookieData forKey:@"ApplicationCookie"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    [self saveCookiesToDefaults];
 }
 
 @end
